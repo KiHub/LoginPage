cd0058913711402e3a6ef2f793b3e40e51a33e61
Kirill Ki <kirillkalapov@gmail.com>
2022-06-04 20:40:50 +0200

    Initial Commit

---
+++ LoginPage/LoginPageApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginPageApp.swift
+//  LoginPage
+//
+//  Created by  Mr.Ki on 04.06.2022.
+//
+
+import SwiftUI
+
+@main
+struct LoginPageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
